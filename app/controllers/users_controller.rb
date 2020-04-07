fcdd4fc6045766f4f7fa015dba7ff844e8afc479
8ceg6 <8ceg6@comcast.net>
2020-04-07 09:01:36 -0400

    set up login get and post with check to make sure user is true

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,19 @@ class UsersController < ApplicationController
         erb:'users/signup'
     end
 
-    # get 'login' do 
+    get '/login' do 
+        erb :"users/login"
+    end 
 
-    # end 
+    post '/login' do 
+            user = User.find_by(username: params[:username])
+            if user && user.authenticate(params[:password])
+                session[:user_id] = user.id 
+                redirect to "/users/#{user.id}"
+            else 
+                redirect to '/'
+            end 
+    end 
 
     post '/signup' do 
         user = User.create(params)
