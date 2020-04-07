febdfc370f15bf0562480b3bccb6c8dc91baee1e
8ceg6 <8ceg6@comcast.net>
2020-04-06 22:15:56 -0400

    set up get and post pages in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,15 @@ class UsersController < ApplicationController
     get "/signup" do 
         erb:'users/signup'
     end
+
+    post '/users' do 
+        @user = User.create(params)
+        redirect to "/users/#{@user.id}"
+    end 
+
+    get '/users/:id' do 
+        binding.pry
+        @user.find_by_id(params[:id])
+        erb :show
+    end 
 end 
