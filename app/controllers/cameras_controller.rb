36ea259b873d91b731f11bd63db8c04b39cd5faf
8ceg6 <8ceg6@comcast.net>
2020-04-08 22:14:39 -0400

    trying to set up delete camera functions so i can remove cameras that where made without a user id

--- app/controllers/cameras_controller.rb
+++ app/controllers/cameras_controller.rb
@@ -2,11 +2,14 @@ class CamerasController < ApplicationController
 
 
     get '/cameras/new' do 
-
-
         erb :'cameras/new'
     end 
 
+    get '/cameras' do 
+        @cameras = Camera.all  
+        @users = User.all
+        erb :'cameras/index'
+    end
     post '/cameras' do 
         camera = Camera.create(params)
         user = Helpers.current_user(session)
@@ -19,6 +22,25 @@ class CamerasController < ApplicationController
 
     get '/cameras/:id' do 
         @camera = Camera.find_by_id(params[:id])
+        if !@camera
+        redirect to '/cameras'
+        else
         erb :'cameras/show'
+        end
     end 
+    
+    get '/cameras/:id/edit' do
+        # if !is_logged_in?
+        # redirect to '/login' 
+        # end
+        @camera = Camera.find_by(params[:id])
+        #  binding.pry
+        # if @tweet.user_id == !current_user.id
+            
+        #     redirect to '/login'
+
+        # else
+          erb :'/cameras/edit'
+        # end
+      end
 end 
